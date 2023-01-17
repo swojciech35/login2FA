ec7ac4c3c3a77b75b15351af8cf379699564e63e
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-01-17 18:23:43 +0100

    login user (without 2fa)

--- user.py
+++ user.py
@@ -1,9 +1,16 @@
 from databaseconnect import insertUser
 from security import hashPassword
-
-
+from databaseconnect import getPassword
+from security import verifyPassword
 def userAdd(login,password):
     passwordEncrypt=hashPassword(password) #szyfrowanie hasła
     insertUser(login,passwordEncrypt)
 
 
+def userLogin(login,password):
+    passwordEnctypted=getPassword(login)
+    verify=verifyPassword(password,passwordEnctypted)
+    if verify:
+        print("zalogowano")
+    else:
+        print("blad logowania")
