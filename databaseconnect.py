b2401573d828b01ab5abbb3353e08d7e974aee0a
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-01-17 18:14:13 +0100

    get password from database

--- databaseconnect.py
+++ databaseconnect.py
@@ -1,18 +1,19 @@
 import psycopg2 as db
 
-def queryDatabase(query):
+def getPassword(login):
 
     con = db.connect(host = "195.150.230.208", port = 5432, database = "2022_nazwisko_imie", user = "2022_nazwisko_imie", password = "*****")
 
 
     cursor = con.cursor()
-    cursor.execute(query)
+    cursor.execute("SELECT password FROM login2fa.user WHERE login = %s",(login,))
     resultset = cursor.fetchall()
     con.close()
-    return resultset
+    return resultset[0][0]
 
 def insertUser(login, password):
     con = db.connect(host="195.150.230.208", port=5432, database="2022_nazwisko_imie", user="2022_nazwisko_imie",password="*****")
+
 
     cursor = con.cursor()
     cursor.execute("INSERT INTO login2fa.user (login, password) VALUES(%s, %s)", (login,password))
